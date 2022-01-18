723436d8f4433e227c6916f9a05eca8ec1e2e990
iuly <esbe6geeb66e@sdfsdffsd.dfs>
2022-01-18 11:25:02 +0200

    added isset added for end task

--- get_exercise.php
+++ get_exercise.php
@@ -1,18 +1,22 @@
 <?php
 session_start();
 
-$number = 3;
 
-if (isset($number)) {
-    echo isset ($number);
+
+$number = $_GET["name"];
+
+if (isset($_GET)) {
+    echo isset ($_GET);
+    echo "<br>";
 }
 
-if (is_int($number)) {
-    for ($i = 1; $i <= $number; $i++) {
-        echo $i += $number;
+if (is_int($_GET)) {
+    for ($i = 1; $i <= $_GET; $i++) {
+        echo $i += $_GET;
     }
-
 }
-
+echo "<br>";
+$_SESSION["previous"] = ($_GET);
+echo $_SESSION["previous"];
 
 ?>
