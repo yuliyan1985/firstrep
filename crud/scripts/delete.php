b8e03e53633b3f77773e7910cf2fd855f1a47e07
yuliyan1985 <udimitrov1985@gmail.com>
2022-02-07 19:11:13 +0200

    added table selected all from number_guess added edit button input update end task

--- crud/scripts/delete.php
+++ crud/scripts/delete.php
@@ -1,18 +1,11 @@
-<!DOCTYPE html>
-<head></head>
-<body>
 
-<form action="../index.php" method="GET">
-    <button class="formelement" type="submit">delete</button>
-</form>
-</body>
-</html>
+<?php
 
-<?
-
-if(isset($_GET['delete'])){
+if(isset($_GET['delete'])) {
     $id = $_GET['delete'];
     $conn->query("DELETE FROM number_guess WHERE id=$id") or die($conn->error());
 }
+
+header("Location: index.php");
 ?>
-<a href="index.php">back</a>
+
