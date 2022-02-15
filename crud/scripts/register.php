04ac8210c8c11e7b20370facfa1aa5f2e15ca095
yuliyan1985 <udimitrov1985@gmail.com>
2022-02-15 10:27:34 +0200

    register

---
+++ crud/scripts/register.php
@@ -0,0 +1,25 @@
+<?php
+
+$sq = "INSERT INTO `login` ( `username`, `password` ) 
+
+VALUES ('" . $_POST['username'] . "','" . $_POST ["password_1"] . "');";
+
+if ($_POST['password_1'] !== ($_POST['password_2'])){
+    header("location:index.php?action=register_forms");
+
+}
+else{
+
+    header("location:index.php");
+}
+$ql = $conn->prepare($sq);
+$ql->execute();
+
+?>
+
+
+
+
+
+
+
