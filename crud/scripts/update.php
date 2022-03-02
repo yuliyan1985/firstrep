365d91bae8b1d7108350a0749b38373aba36d30e
yuliyan1985 <udimitrov1985@gmail.com>
2022-03-02 12:53:22 +0200

    added table selected all from number_guess added edit button input update end task login logout register points point added readme added 5 logic points added another 2 logic points corrected 7 point point 5 updatе 2 new points added implode insert files added connection insert in_array

--- crud/scripts/update.php
+++ crud/scripts/update.php
@@ -6,9 +6,7 @@ $computer_name = $_POST['computer_number'];
 $win_loss = $_POST['win_loss'];
 
 $query = "UPDATE number_guess SET  user_number = '$user_name', computer_number = '$computer_name' ,win_loss = '$win_loss'  WHERE id='$id' ";
-
 $query_run = $conn->prepare($query);
-
 if ($query_run->execute() === TRUE) {
     echo "Record updated successfully";
 } else {
--- scan/index.php
+++ scan/index.php
@@ -1,13 +1,42 @@
 <?php
 require_once 'db.php';
 
-$files = array_diff (scandir('images'), array('..', '.'));
-foreach($files as $file) {
-        $query = "INSERT IGNORE INTO `files` ( `name` )
-        VALUES ('" . $file .  "');";
+$bagWithballs = array_diff(scandir('images'), array('..', '.'));
+
+$sql = "SELECT `name` FROM `files`";
+$statement = $conn->query($sql);
+$bagWithBoxAndBalls = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($bagWithballs as $balls) {
+        $shouldInsert = true;
+
+        foreach ($bagWithBoxAndBalls as $row) {
+
+            if (in_array($balls, $row)) {
+
+                $shouldInsert = false;
+                break;
+            }
+        }
+        if ($shouldInsert === true) {
+        $query = "INSERT  INTO `files` ( `name` )
+         VALUES ('" . $balls . "');";
         $query_run = $conn->prepare($query);
-        $query_run ->execute();
+        $query_run->execute();
+
+    }
 }
 
+
+
 ?>
+
+
+
+
+
+
+
+
+
 
