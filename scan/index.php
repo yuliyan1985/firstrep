698374cf2f0d43ad9863acc15a645e622ac723c8
yuliyan1985 <udimitrov1985@gmail.com>
2022-03-04 16:46:05 +0200

    added table selected all from number_guess added edit button input update end task login logout register points point added readme added 5 logic points added another 2 logic points corrected 7 point point 5 updatе 2 new points added implode insert files added connection insert in_array 4 if

--- scan/index.php
+++ scan/index.php
@@ -1,32 +1,43 @@
 <?php
+
+
+
+
+
+
 require_once 'db.php';
 
-$bagWithballs = array_diff(scandir('images'), array('..', '.'));
+$bagBalls = array_diff(scandir('images'), array('..', '.'));
 
 $sql = "SELECT `name` FROM `files`";
 $statement = $conn->query($sql);
-$bagWithBoxAndBalls = $statement->fetchAll(PDO::FETCH_ASSOC);
+$bagBoxBalls = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-    foreach ($bagWithballs as $balls) {
-        $shouldInsert = true;
-
-        foreach ($bagWithBoxAndBalls as $row) {
-
-            if (in_array($balls, $row)) {
-
-                $shouldInsert = false;
-                break;
-            }
-        }
-        if ($shouldInsert === true) {
-        $query = "INSERT  INTO `files` ( `name` )
-         VALUES ('" . $balls . "');";
-        $query_run = $conn->prepare($query);
-        $query_run->execute();
-
-    }
+if (empty($bagBoxBalls)) {
+    $query = "INSERT  INTO `files` ( `name` )
+        VALUES ('" . $bagBalls . "');";
+    $query_run = $conn->prepare($query);
+    $query_run->execute();
 }
 
+if (!empty($bagBoxBalls)) {
+
+    foreach ($bagBoxBalls as $row) {
+
+       if (!in_array($row['name'], $bagBalls)) {
+
+           $query = "INSERT  INTO `files` ( `name` )
+             VALUES ('" . $bagBalls . "');";
+           $query_run = $conn->prepare($query);
+           $query_run->execute();
+
+        }
+
+       if (in_array($row['name'], $bagBalls)){
+           break;
+       }
+    }
+}
 
 
 ?>
