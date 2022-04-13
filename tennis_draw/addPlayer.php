b73e3e2bf05ea878767202b7709924e8805e4fb7
yuliyan1985 <udimitrov1985@gmail.com>
2022-04-13 16:26:27 +0300

    winner

---
+++ tennis_draw/addPlayer.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<head>
+
+</head>
+<body>
+
+<form action="tennis_table.php" method="POST"
+
+<input type="text" class="form control required" name="start" placeholder="add player"
+    <br>
+
+<button id="submit-button" type="submit" class="btn btn-success btn-lg btn-block">
+    <i class="icon-check2"></i> add
+</button>
+
+</form>
+
+
+</body>
+
+</html>
